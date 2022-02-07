1c63be5d1299e850da1b9651edd019f9f94f4761
Nikhil George <56214901+thegeorgenikhil@users.noreply.github.com>
2022-02-08 00:39:09 +0530

    add: ratings component

---
+++ components/Rating/rating.js
@@ -0,0 +1,14 @@
+const ratingStars = document.getElementsByClassName("rating-star");
+for (let i = 0;i < ratingStars.length;i++){
+    ratingStars[i].addEventListener("click",(e) => {
+        const id = ratingStars[i].id;
+        const idValue = id.slice(id.length-1);
+        console.log(idValue)
+        for (let j = 4;j >= 5-idValue;j--){
+            ratingStars[j].classList.add("rated")
+        }
+        for (let j = 4 - idValue;j >= 0;j--){
+            ratingStars[j].classList.remove("rated")
+        }
+    })
+}
--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,11 @@
 const sideNav = document.getElementById("side-nav")
 const sideNavToggle = document.getElementById("sidenav-toggle")
+const sideNavToggleIcon = document.querySelector(".sidenav-toggle i")
 const sideNavOverlay = document.getElementById("sidenav-overlay")
 
 
 sideNavToggle.addEventListener("click",() => {
     sideNav.classList.toggle("sidenav-show");
-    sideNavOverlay.classList.toggle("show")
+    sideNavOverlay.classList.toggle("show");
+    sideNavToggleIcon.classList.toggle("fa-times")
 })
