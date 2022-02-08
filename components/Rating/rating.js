3362e07a748060ab27549e09504fe6b97adccf69
Nikhil George <56214901+thegeorgenikhil@users.noreply.github.com>
2022-02-08 13:21:53 +0530

    fix: removed console.log from ratings.js

--- components/Rating/rating.js
+++ components/Rating/rating.js
@@ -3,7 +3,6 @@ for (let i = 0;i < ratingStars.length;i++){
     ratingStars[i].addEventListener("click",(e) => {
         const id = ratingStars[i].id;
         const idValue = id.slice(id.length-1);
-        console.log(idValue)
         for (let j = 4;j >= 5-idValue;j--){
             ratingStars[j].classList.add("rated")
         }
